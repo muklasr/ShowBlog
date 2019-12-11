aa77544340bce77b75c2daaee684f6b55b070068
muklasr <muclasrachmanto@gmail.com>
2019-12-11 17:38:21 +0700

    Bismillah

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,25 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', 'HomeController@index');
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/dashboard', 'HomeController@dashboard');
+
+    Route::get('/category', 'CategoryController@index');
+    Route::post('/category/add', 'CategoryController@store')->name('categoryAdd');
+    Route::get('/category/edit/{id}', 'CategoryController@edit')->name('categoryEdit');
+    Route::post('/category/update', 'CategoryController@update')->name('categoryUpdate');
+    Route::get('/category/delete/{id}', 'CategoryController@destroy')->name('categoryDelete');
+
+    Route::get('/article', 'ArticleController@index');
+    Route::post('/article/add', 'ArticleController@store')->name('articleAdd');
+    Route::get('/article/edit/{id}', 'ArticleController@edit')->name('articleEdit');
+    Route::post('/article/update', 'ArticleController@update')->name('articleUpdate');
+    Route::get('/article/delete/{id}', 'ArticleController@destroy')->name('articleDelete');
 });
+
+Route::get('/{id}', 'HomeController@detail');
